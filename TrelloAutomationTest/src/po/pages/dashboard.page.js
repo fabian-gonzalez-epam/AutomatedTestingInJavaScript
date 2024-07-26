d21ef865b53ac62735b80eb373bfa3daca7d339c
fabian_gonzalez@gmail.com <fabian_gonzalez@gmail.com>
2024-07-25 21:48:30 -0600

    Format Testing codes

--- TrelloAutomationTest/src/po/pages/dashboard.page.js
+++ TrelloAutomationTest/src/po/pages/dashboard.page.js
@@ -1,8 +1,7 @@
-const BasePage = require("./base.page");
-const {BoardsMenu, Header, AddBoard, SideMenu} = require('./../components')
+const BasePage = require('./base.page');
+const { BoardsMenu, Header, AddBoard, SideMenu } = require('./../components');
 
 class DashboardPage extends BasePage {
-    
     constructor() {
         super('https://trello.com/u/fabiangonzalez195/boards');
         this.boardsMenu = new BoardsMenu();
--- TrelloAutomationTest/src/screenplay/actors/epamer.actor.js
+++ TrelloAutomationTest/src/screenplay/actors/epamer.actor.js
@@ -1,14 +1,14 @@
-const BaseActor = require("../actors/base.actor");
-const Interactions = require("../actions/interactions");
-const Interrogations = require("../actions/interrogations");
+const BaseActor = require('../actors/base.actor');
+const Interactions = require('../actions/interactions');
+const Interrogations = require('../actions/interrogations');
 
 class EpamerActor extends BaseActor {
     constructor() {
-        super("fabian_gonzalez@epam.com", "AFmt&!UGeY6aQ#")
+        super('fabian_gonzalez@epam.com', 'AFmt&!UGeY6aQ#');
         //this.interaction = new Interactions()
         //this.interrogation = new Interrogations()
     }
-    
+
     checkBoardExist() {
         this.interrogation.checkBoardExist();
     }
