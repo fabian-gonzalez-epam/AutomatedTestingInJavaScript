fe06ffa7b624e4761ef90d26e80bb82424dc2b11
fabian-gonzalez-epam <fabian_gonzalez@epam.com>
2024-07-25 20:48:11 -0600

    Test

--- TrelloAutomationTest/src/po/pages/board.page.js
+++ TrelloAutomationTest/src/po/pages/board.page.js
@@ -1,8 +1,13 @@
-const BasePage = require("./base.page");
-const {BoardsMenu, Header, AddBoard, SideMenu, BoardCanvas} = require('../components')
+const BasePage = require('./base.page');
+const {
+    BoardsMenu,
+    Header,
+    AddBoard,
+    SideMenu,
+    BoardCanvas,
+} = require('../components');
 
 class BoardPage extends BasePage {
-    
     constructor() {
         super('https://trello.com/b/667f071e12780f21b2713d28');
         this.boardsMenu = new BoardsMenu();
