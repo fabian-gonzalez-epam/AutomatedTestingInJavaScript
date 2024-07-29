5f465db6eecbe38aaa99c2191a77ffbee5c3224e
fabian_gonzalez@gmail.com <fabian_gonzalez@gmail.com>
2024-07-29 15:15:46 -0600

    Format Testing codes

--- TrelloAutomationTest/src/screenplay/actors/index.js
+++ TrelloAutomationTest/src/screenplay/actors/index.js
@@ -1,17 +1,16 @@
 const EpamerActor = require('./epamer.actor');
 const NonEpamerActor = require('./non-epamer.actor');
 
-function actor(name){
+function actor(name) {
     const items = {
         epamer: new EpamerActor(),
-        student: new NonEpamerActor()
-        
-    }
+        student: new NonEpamerActor(),
+    };
     return items[name.toLowerCase()];
 }
 
 module.exports = {
     EpamerActor,
     NonEpamerActor,
-    actor
-}
+    actor,
+};
