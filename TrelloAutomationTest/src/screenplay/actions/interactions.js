369c459211b7bb878f8fc7a9a196146b1a34662c
fabian-gonzalez-epam <fabian_gonzalez@epam.com>
2024-07-29 14:24:21 -0600

    Preparation

--- TrelloAutomationTest/src/screenplay/actions/interactions.js
+++ TrelloAutomationTest/src/screenplay/actions/interactions.js
@@ -6,18 +6,18 @@ class Interactions {
         await pages('login').loginForm.input('username').waitForDisplayed();
         await pages('login').loginForm.input('username').waitForEnabled();
         await pages('login').loginForm.input('username').setValue(username);
-
+        
         await pages('login').loginForm.submitButton.waitForDisplayed();
         await pages('login').loginForm.submitButton.waitForEnabled();
         await pages('login').loginForm.submitButton.click();
-
+        
         await pages('login').loginForm.input('password').waitForDisplayed();
         await pages('login').loginForm.input('password').waitForEnabled();
         await pages('login').loginForm.input('password').setValue(password);
-
+        
         await pages('login').loginForm.submitButton.waitForDisplayed();
         await pages('login').loginForm.submitButton.waitForEnabled();
-        await pages('login').loginForm.submitButton.click();
+        await pages('login').loginForm.submitButton.click(); 
 
         /* $("//button[@data-testid='header-create-menu-button']").waitForDisplayed())
         $("//button[@data-testid='header-create-menu-button']").waitForEnabled())
@@ -28,24 +28,21 @@ class Interactions {
         $("//input[@data-testid='create-board-title-input']").waitForDisplayed())
         $("//input[@data-testid='create-board-title-input']").waitForEnabled())
         $("//input[@data-testid='create-board-title-input']").setValue("Board")) */
+         
+        
     }
-
+    
     async addBoard() {
-        await $(
-            "//button[@data-testid='header-create-menu-button']"
-        ).waitForDisplayed();
-        await $(
-            "//button[@data-testid='header-create-menu-button']"
-        ).waitForEnabled();
-        await $("//button[@data-testid='header-create-menu-button']").click();
-        $("//span[text()='Create board']").waitForDisplayed();
-        $("//span[text()='Create board']").waitForEnabled();
-        $("//span[text()='Create board']").click();
-        $(
-            "//input[@data-testid='create-board-title-input']"
-        ).waitForDisplayed();
-        $("//input[@data-testid='create-board-title-input']").waitForEnabled();
-        $("//input[@data-testid='create-board-title-input']").setValue('Board');
+        await $("//button[@data-testid='header-create-menu-button']").waitForDisplayed()
+        await $("//button[@data-testid='header-create-menu-button']").waitForEnabled()
+        await $("//button[@data-testid='header-create-menu-button']").click()
+        $("//span[text()='Create board']").waitForDisplayed()
+        $("//span[text()='Create board']").waitForEnabled()
+        $("//span[text()='Create board']").click()
+        $("//input[@data-testid='create-board-title-input']").waitForDisplayed()
+        $("//input[@data-testid='create-board-title-input']").waitForEnabled()
+        $("//input[@data-testid='create-board-title-input']").setValue("Board")
+
 
         /* pages('dashboard').header.addButton.waitForDisplayed()
         pages('dashboard').header.addButton.aitForEnabled()
